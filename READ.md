ffb19716aea46a35e5acb8220c3f6412017bd862
Milan-Zlamal <82092210+Milan-Zlamal@users.noreply.github.com>
2022-10-19 13:26:45 +0200

    try

---
+++ READ.md
@@ -0,0 +1,6 @@
+This not work - 2 tryes one is main - not finding index.html
+second is not ready for run
+
+target made simple create, read, update database with sqlite SQLAlchemy and Flask - some problem with adress to the index.html or with him
+
+need to try it in kids steps another time
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from flask import Flask, request, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -10,11 +9,13 @@ database_file = "sqlite:///{}".format(os.path.join(project_dir, "todo.db"))
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 db = SQLAlchemy(app)
 
+
 class Note(db.Model):
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     text = db.Column(db.Text)
     done = db.Column(db.Boolean)
-    dataAdded = db.Column(db.DateTime, default = datetime.now())
+    dateAdded = db.Column(db.DateTime, default=datetime.now())
+
 
 def create_note(text):
     note = Note(text=text)
@@ -22,8 +23,10 @@ def create_note(text):
     db.session.commit()
     db.session.refresh(note)
 
+
 def read_notes():
     return db.session.query(Note).all()
+
 
 def update_note(note_id, text, done):
     db.session.query(Note).filter_by(id=note_id).update({
@@ -32,26 +35,27 @@ def update_note(note_id, text, done):
     })
     db.session.commit()
 
+
 def delete_note(note_id):
     db.session.query(Note).filter_by(id=note_id).delete()
     db.session.commit()
 
-# def endpoint in flask
-@app.route("/", methods=["POST","GET"])
+
+@app.route("/", methods=["POST", "GET"])
 def view_index():
     if request.method == "POST":
         create_note(request.form['text'])
     return render_template("index.html", notes=read_notes())
 
-@app.route("/edit/<note_id>", methods = ["POST", "GET"])
-def edit_note(note_id):
-    if request.method == "POST":
-        update_note(note_id, text=request.form['text'], done=request.form['done'])
-    elif request.method == "GET":
-        delete_note(note_id)
-    return redirect("/", code=302)
+
+# @app.route("/edit/<note_id>", methods=["POST", "GET"])
+# def edit_note(note_id):
+#     if request.method == "POST":
+#         update_note(note_id, text=request.form['text'], done=request.form['done'])
+#     elif request.method == "GET":
+#         delete_note(note_id)
+#     return redirect("/", code=302)
 
 if __name__ == "__main__":
     db.create_all()
     app.run(debug=True)
-
